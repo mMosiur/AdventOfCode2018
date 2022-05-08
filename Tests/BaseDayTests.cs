6bf768f807e62ade7a938d3c1a6692fe3f2a49ff
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-05-08 18:32:54 +0200

    Added Tests project + day01 tests

---
+++ Tests/BaseDayTests.cs
@@ -0,0 +1,44 @@
+using AdventOfCode.Abstractions;
+using Xunit;
+
+namespace AdventOfCode.Year2018.Tests;
+
+public abstract class BaseDayTests<T> where T : DaySolver
+{
+	public const string BaseInputsDirectory = "Inputs";
+	public abstract string DayInputsDirectory { get; }
+
+	/// <summary>
+	/// We can't specify parametrized constraint for T argument, so we have to use this workaround.
+	/// Subclasses should override this property with simply "return new <T>(inputFilePath);".
+	/// </summary>
+	protected abstract T CreateSolver(string inputFilePath);
+
+	private string GetInputFilePath(string inputFilename)
+	{
+		string filepath = Path.Combine(
+			BaseInputsDirectory,
+			DayInputsDirectory,
+			inputFilename
+		);
+		bool testFileExists = File.Exists(filepath);
+		Assert.True(testFileExists, $"Specified test input file does not exist: {filepath}");
+		return filepath;
+	}
+
+	public virtual void TestPart1(string inputFilename, string expectedResult)
+	{
+		string filepath = GetInputFilePath(inputFilename);
+		var solver = CreateSolver(filepath);
+		string result = solver.SolvePart1();
+		Assert.Equal(expectedResult, result);
+	}
+
+	public virtual void TestPart2(string inputFilename, string expectedResult)
+	{
+		string filepath = GetInputFilePath(inputFilename);
+		var solver = CreateSolver(filepath);
+		string result = solver.SolvePart2();
+		Assert.Equal(expectedResult, result);
+	}
+}
