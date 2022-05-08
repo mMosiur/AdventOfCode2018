fcda2f7947430f6dde19358145e88fefaa0a8716
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-05-08 20:19:27 +0200

    Added day02 solution

---
+++ Day02 - Inventory Management System/Day02Solver.cs
@@ -0,0 +1,62 @@
+using AdventOfCode.Abstractions;
+
+namespace AdventOfCode.Year2018.Day02;
+
+public class Day02Solver : DaySolver
+{
+	public Day02Solver(string inputFilePath) : base(inputFilePath)
+	{
+	}
+
+	public override string SolvePart1()
+	{
+		int twos = 0;
+		int threes = 0;
+		var analyzer = new CharAnalyzer();
+		foreach (string line in InputLines)
+		{
+			analyzer.Chars = line;
+			if (analyzer.GetLettersThatAppearNTimes(2).Any())
+			{
+				twos++;
+			}
+			if (analyzer.GetLettersThatAppearNTimes(3).Any())
+			{
+				threes++;
+			}
+		}
+		int checksum = twos * threes;
+		return checksum.ToString();
+	}
+
+	private (string First, string Second, int DifferencePosition) GetFirstPairThatDiffersByOne()
+	{
+		var lines = InputLines.ToList();
+		var analyzer = new CharAnalyzer();
+		for (int i = 0; i < lines.Count; i++)
+		{
+			string line = lines[i];
+			analyzer.Chars = line;
+			for (int j = i + 1; j < lines.Count; j++)
+			{
+				string other = lines[j];
+				var positions = analyzer.GetPositionsOfDifferences(other);
+				int differences = positions.Count();
+				if (differences == 1)
+				{
+					return (line, other, positions.First());
+				}
+			}
+		}
+		throw new ApplicationException("No pair that differs by 1 found");
+	}
+
+	public override string SolvePart2()
+	{
+		var pair = GetFirstPairThatDiffersByOne();
+		string result =
+			pair.First.Substring(0, pair.DifferencePosition)
+			+ pair.First.Substring(pair.DifferencePosition + 1);
+		return result;
+	}
+}
