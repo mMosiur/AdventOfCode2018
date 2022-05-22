15c22e5d19d329c09e864723c8cb10b6776aa1a4
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-05-22 23:47:33 +0200

    Applied code style static analysis

--- Day05 - Alchemical Reduction/Helpers.cs
+++ Day05 - Alchemical Reduction/Helpers.cs
@@ -4,7 +4,7 @@ public static class Helpers
 {
 	public static IEnumerable<(char Lower, char Upper)> GetAsciiAlphabetCasePairs()
 	{
-		for(char upper = 'A'; upper <= 'Z'; upper++)
+		for (char upper = 'A'; upper <= 'Z'; upper++)
 		{
 			char lower = (char)(upper + 32);
 			yield return (lower, upper);
