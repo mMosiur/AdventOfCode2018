b8a7aa42a974a1945a6af0990a1f25493c0898b1
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-06-19 13:14:47 +0200

    Added day 10 part 2 tests

--- Tests/Day10Tests.cs
+++ Tests/Day10Tests.cs
@@ -50,4 +50,10 @@ public class Day10Tests : BaseDayTests<Day10Solver, Day10SolverOptions>
 	[MemberData(nameof(TestPart1Data))]
 	public override void TestPart1(string inputFilename, string expectedResult, Day10SolverOptions? options = null)
 		=> base.TestPart1(inputFilename, expectedResult, options);
+
+	[Theory]
+	[InlineData("example-input.txt", "3")]
+	[InlineData("my-input.txt", "10243")]
+	public override void TestPart2(string inputFilename, string expectedResult, Day10SolverOptions? options = null)
+		=> base.TestPart2(inputFilename, expectedResult, options);
 }
