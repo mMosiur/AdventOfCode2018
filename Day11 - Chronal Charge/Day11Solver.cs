61fd67057071c13419f7bb9d607ad92534c42159
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-07-03 13:10:53 +0200

    Added boilerplate day solution code

---
+++ Day11 - Chronal Charge/Day11Solver.cs
@@ -0,0 +1,28 @@
+using AdventOfCode.Abstractions;
+
+namespace AdventOfCode.Year2022.Day11;
+
+public class Day11Solver : DaySolver
+{
+	public Day11Solver(Day11SolverOptions options) : base(options)
+	{
+		// Initialize Day11 solver here.
+		// Property `Input` contains the raw input text.
+		// Property `InputLines` enumerates lines in the input text.
+	}
+
+	public Day11Solver(Action<Day11SolverOptions>? configure = null)
+		: this(DaySolverOptions.FromConfigureAction(configure))
+	{
+	}
+
+	public override string SolvePart1()
+	{
+		return "UNSOLVED";
+	}
+
+	public override string SolvePart2()
+	{
+		return "UNSOLVED";
+	}
+}
