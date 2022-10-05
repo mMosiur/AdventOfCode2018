1ac5f1fec177a077440331ea541b1928dbdd7ece
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-10-05 11:05:47 +0200

    Added day 19 part two tests

--- Tests/Day19Tests.cs
+++ Tests/Day19Tests.cs
@@ -1,4 +1,5 @@
 using AdventOfCode.Year2018.Day19;
+using AdventOfCode.Year2018.Day19.Device.CPUs;
 using Xunit;
 
 namespace AdventOfCode.Year2018.Tests;
@@ -16,4 +17,19 @@ public class Day19Tests : BaseDayTests<Day19Solver, Day19SolverOptions>
 	[InlineData("my-input.txt", "1430")]
 	public override void TestPart1(string inputFilename, string expectedResult, Day19SolverOptions? options = null)
 		=> base.TestPart1(inputFilename, expectedResult, options);
+
+	[Theory]
+	[InlineData("example-input.txt")]
+	public void TestPart2FailsOnWrongInput(string inputFilename)
+	{
+		string inputFilepath = GetInputFilepath(inputFilename);
+		Day19SolverOptions options = new() { InputFilepath = inputFilepath };
+		Day19Solver solver = CreateSolver(options);
+		Assert.Throws<RiggedCPUException>(() => solver.SolvePart2());
+	}
+
+	[Theory]
+	[InlineData("my-input.txt", "14266944")]
+	public override void TestPart2(string inputFilename, string expectedResult, Day19SolverOptions? options = null)
+		=> base.TestPart2(inputFilename, expectedResult, options);
 }
