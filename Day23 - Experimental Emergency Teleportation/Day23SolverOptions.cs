ecb5146f78f055c2bcd3fb5a9cb68eaeb23d1cae
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-11-07 11:55:55 +0100

    Add origin position to day solver options

--- Day23 - Experimental Emergency Teleportation/Day23SolverOptions.cs
+++ Day23 - Experimental Emergency Teleportation/Day23SolverOptions.cs
@@ -5,4 +5,8 @@ namespace AdventOfCode.Year2018.Day23;
 public sealed class Day23SolverOptions : DaySolverOptions
 {
 	public static Day23SolverOptions Default => new();
+
+	public int MyPositionX { get; set; } = 0;
+	public int MyPositionY { get; set; } = 0;
+	public int MyPositionZ { get; set; } = 0;
 }
