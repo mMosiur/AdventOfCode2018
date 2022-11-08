a1759625ae8178857c70acf2ad0d1545a66532b6
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-11-08 19:41:50 +0100

    Added day 23 part two tests

--- Tests/Day23Tests.cs
+++ Tests/Day23Tests.cs
@@ -12,8 +12,14 @@ public class Day23Tests : BaseDayTests<Day23Solver, Day23SolverOptions>
 	protected override Day23Solver CreateSolver(Day23SolverOptions options) => new(options);
 
 	[Theory]
-	[InlineData("example-input.txt", "7")]
+	[InlineData("example-input-1.txt", "7")]
 	[InlineData("my-input.txt", "616")]
 	public override void TestPart1(string inputFilename, string expectedResult, Day23SolverOptions? options = null)
 		=> base.TestPart1(inputFilename, expectedResult, options);
+
+	[Theory]
+	[InlineData("example-input-2.txt", "36")]
+	[InlineData("my-input.txt", "93750870")]
+	public override void TestPart2(string inputFilename, string expectedResult, Day23SolverOptions? options = null)
+		=> base.TestPart2(inputFilename, expectedResult, options);
 }
